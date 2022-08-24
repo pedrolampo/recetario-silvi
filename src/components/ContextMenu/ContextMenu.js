6f4738b3722081c88a3f5d3182a156595ccec4b3
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-08-24 20:20:14 -0300

    ui fix

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -6,8 +6,8 @@ import './contextMenu.css';
 export default function ContextMenu({ active, recipeId }) {
   return (
     <div className={active ? 'context-menu active' : 'context-menu'}>
-      <Link to={`/updateRecipe/${recipeId}`}>
-        <div className="context-menu-btn">Modificar</div>
+      <Link className="context-menu-btn" to={`/updateRecipe/${recipeId}`}>
+        Modificar
       </Link>
       <div className="context-menu-btn">Eliminar</div>
     </div>
